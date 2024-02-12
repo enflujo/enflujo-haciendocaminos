207a961590e7d02727faa313e0030b2cd13cf5e8
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-02-12 16:24:45 -0500

    ✅ Actualizar mapa en selección

--- src/utilidades/cerebro.ts
+++ src/utilidades/cerebro.ts
@@ -1,10 +1,13 @@
 import { atom, map, onMount } from 'nanostores';
 import { pedirDatos } from './ayudas';
 import type { Listas, Ficha, Proyecto } from '@/tipos';
+import type { FeatureCollection } from 'geojson';
 
 export const datosProyectos = atom<Proyecto[]>([]);
 export const datosFicha = map<Ficha>({ visible: false });
 export const datosListas = map<Listas>();
+export const geo = map<FeatureCollection>();
+let _copiaDatosMapa: FeatureCollection;
 
 onMount(datosListas, () => {
   pedirDatos<Listas>(`${import.meta.env.BASE_URL}/listas.json`).then((res) => {
@@ -15,3 +18,17 @@ onMount(datosListas, () => {
     });
   });
 });
+
+onMount(geo, () => {
+  pedirDatos<FeatureCollection>(`${import.meta.env.BASE_URL}/datosMapa.geo.json`).then((res) => {
+    geo.set(res);
+    _copiaDatosMapa = res;
+  });
+});
+
+export function filtrarMapa(lugares: string[]) {
+  const lugaresFiltrados = _copiaDatosMapa?.features.filter((lugar) => lugares.includes(lugar.properties?.slug));
+  if (lugaresFiltrados) {
+    geo.setKey('features', lugaresFiltrados);
+  }
+}
