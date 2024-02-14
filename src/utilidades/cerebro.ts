98cb7bd2224a6ce3d3fa9aa64d29abdd074be917
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-02-14 14:23:15 -0500

    🎉 Navegación entre fichas

--- src/utilidades/cerebro.ts
+++ src/utilidades/cerebro.ts
@@ -1,12 +1,13 @@
 import { atom, map, onMount } from 'nanostores';
-import { pedirDatos } from './ayudas';
-import type { Listas, Ficha, Proyecto } from '@/tipos';
+import { ordenarListaObjetos, pedirDatos } from './ayudas';
+import type { Listas, Ficha, Proyecto, ELementoProyecto, RelacionesFicha, ElementoFicha } from '@/tipos';
 import type { FeatureCollection } from 'geojson';
 
 export const datosProyectos = atom<Proyecto[]>([]);
 export const datosFicha = map<Ficha>({ visible: false });
 export const datosListas = map<Listas>();
 export const geo = map<FeatureCollection>();
+export const elementoSeleccionado = map<{ tipo: string; id: string; lista: string }>();
 let _copiaDatosMapa: FeatureCollection;
 
 onMount(datosListas, () => {
@@ -32,3 +33,90 @@ export function filtrarMapa(lugares: string[]) {
     geo.setKey('features', lugaresFiltrados);
   }
 }
+
+elementoSeleccionado.subscribe((elemento) => {
+  if (!Object.keys(elemento).length) return;
+
+  const { tipo, id, lista } = elemento;
+
+  if (tipo === 'proyecto') {
+    const datosProyecto = datosProyectos.get()[+id];
+
+    if (!datosProyecto) return;
+
+    datosFicha.set({
+      visible: true,
+      lista: 'Proyecto',
+      id: datosProyecto.id,
+      titulo: datosProyecto.nombre.nombre,
+      descripcion: datosProyecto.descripcion ? datosProyecto.descripcion : '',
+      paises: datosProyecto.paises ? datosProyecto.paises : [],
+      categorias: datosProyecto.categorias ? [datosProyecto.categorias] : [],
+      lideres: datosProyecto.lideres ? datosProyecto.lideres : [],
+      roles: datosProyecto.roles ? [datosProyecto.roles] : [],
+      participantes: datosProyecto.participantes ? datosProyecto.participantes : [],
+      ramas: datosProyecto.ramas ? datosProyecto.ramas : [],
+      temas: datosProyecto.temas ? datosProyecto.temas : [],
+      objetos: datosProyecto.objetos ? datosProyecto.objetos : [],
+      municipios: datosProyecto.municipios ? datosProyecto.municipios : [],
+      decadas: datosProyecto.decadas ? datosProyecto.decadas : [],
+      enlaces: datosProyecto.enlaces ? datosProyecto.enlaces : [],
+      imagenes: datosProyecto.imagenes ? datosProyecto.imagenes : []
+    });
+  } else {
+    const listas = datosListas.value;
+    if (!listas) return;
+    const datos = listas[tipo as keyof Listas][+id];
+
+    if (datos) {
+      const proyectos = datos.proyectos?.reduce((lista: ELementoProyecto[], indiceProyecto) => {
+        const proyecto = datosProyectos.value?.find((p) => p.id === indiceProyecto);
+        if (proyecto) {
+          lista.push({ nombre: proyecto.nombre.nombre, id: proyecto.id });
+        }
+
+        ordenarListaObjetos(lista, 'nombre', true);
+        return lista;
+      }, []);
+
+      const relaciones: RelacionesFicha = datos.relaciones.reduce(
+        (lista: { [llave: string]: ElementoFicha[] }, valorActual) => {
+          if (!lista[valorActual.tipo]) {
+            lista[valorActual.tipo] = [];
+          }
+
+          lista[valorActual.tipo].push({
+            slug: valorActual.slug,
+            conteo: valorActual.conteo,
+            nombre: listas[valorActual.tipo as keyof Listas][valorActual.indice].nombre
+          });
+
+          ordenarListaObjetos(lista[valorActual.tipo], 'slug', true);
+          return lista;
+        },
+        {}
+      );
+
+      const lugaresMapa = datos.relaciones.filter((relacion) => relacion.tipo === 'municipios');
+      filtrarMapa(lugaresMapa.map((lugar) => lugar.slug));
+
+      datosFicha.set({
+        visible: true,
+        lista,
+        titulo: datos.nombre,
+        conteo: `${datos.conteo}`,
+        paises: relaciones.paises ? relaciones.paises : [],
+        categorias: relaciones.categorias ? relaciones.categorias : [],
+        lideres: relaciones.lideres ? relaciones.lideres : [],
+        roles: relaciones.roles ? relaciones.roles : [],
+        participantes: relaciones.participantes ? relaciones.participantes : [],
+        ramas: relaciones.ramas ? relaciones.ramas : [],
+        temas: relaciones.temas ? relaciones.temas : [],
+        objetos: relaciones.objetos ? relaciones.objetos : [],
+        municipios: relaciones.municipios ? relaciones.municipios : [],
+        decadas: relaciones.decadas ? relaciones.decadas : [],
+        proyecto: proyectos
+      });
+    }
+  }
+});
