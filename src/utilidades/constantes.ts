f7f6948abb31bcd27be4373c0c64c165fe3cfa1a
anattolia <anattolia@gmail.com>
2024-04-08 12:01:04 -0500

    ✨ Corrección título y dependencia

--- src/utilidades/constantes.ts
+++ src/utilidades/constantes.ts
@@ -1,3 +1,3 @@
-export const nombrePagina = 'Haciendo Caminos';
+export const nombrePagina = '60 Años Haciendo Caminos | Departamento de Antropología';
 export const descripcionGeneral =
   'La historia del Departamento de Antropología de la Universidad de Los Andes contada a través de varios de los proyectos de investigación que sus profesorxs y estudiantxs han realizado desde su creación en 1964.';
